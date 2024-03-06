d733af24d03ede13b9db31ecb45847c4bfe9f061
aquibiqbal65 <92910276+aquibiqbal65@users.noreply.github.com>
2024-03-06 23:55:22 +0000

    02_dataTypes

---
+++ Basics/02_dataTypes.js
@@ -0,0 +1,20 @@
+"use strict"; // treat al JS code as newer version
+
+
+//alert(3+3) //popup , only works in browser
+
+ let name = "Aquib"
+ let age = 20
+ let isLoggedIn = false
+
+ //number = 2
+ //bigint
+ //string = ""
+ //boolean = true/false
+ //null = standalone value
+ // undefined = value is not yet assigned
+ //symbol = unique
+
+
+ console.log(typeof null);
+ console.log(typeof undefined);
