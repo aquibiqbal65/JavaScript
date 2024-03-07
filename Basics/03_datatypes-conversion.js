1fabb320c7b87141fd89df83be18300a26a235b4
aquibiqbal65 <92910276+aquibiqbal65@users.noreply.github.com>
2024-03-07 00:15:39 +0000

    operations

--- Basics/03_datatypes-conversion.js
+++ Basics/03_datatypes-conversion.js
@@ -10,3 +10,15 @@ console.log(typeof valueInNumber);
 // "33"= 33
 // "33abc"= NaN
 // true = 1
+
+
+//*************** Operations ************** */
+
+let str1 ="Hello"
+let str2 =" Aquib"
+let str3= str1+str2
+console.log(str3)
+
+let gamecounter=100
+++gamecounter
+console.log(gamecounter);
