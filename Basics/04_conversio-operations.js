3ab3f4e939d1c02266fd95e1e1c362274e98cdfc
aquibiqbal65 <92910276+aquibiqbal65@users.noreply.github.com>
2024-03-09 08:24:24 +0000

    comparisons

---
+++ Basics/04_conversio-operations.js
@@ -0,0 +1,14 @@
+str1="hii"
+str2="Aquib"
+str3=str1+str2
+console.log(str3)
+
+console.log("1" + 2)  // here it will print 1 and then 2
+console.log(1+2+"2")   // here it will first evaluate th expression the treat the string as it is
+
+//if there is string in first,it will treat everything as string
+
+console.log(+true);
+
+let num1, num2, num3
+num1=num2=num3=2+2;  
