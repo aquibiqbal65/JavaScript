3b3ce6b2313edc26d57328e3d5c289eb93a51147
Aquib Iqbal <aquibiqbal65@gmail.com>
2024-03-26 22:22:24 +0000

    created buttons

--- 08_GuessTheNumber/javascript.js
+++ 08_GuessTheNumber/javascript.js
@@ -4,4 +4,44 @@ const submit=document.querySelector('#subt');
 const userInput = document.querySelector('#guessField');
 const guessSlot = document.querySelector('.guesses');
 const remaining = document.querySelector('.lastResult');
+const lowOrHi = document.querySelector('.lowOrHi');
+const StartOver = document.querySelector('.resultParas');
 
+const p = document.createElement('p')
+
+let preGuess = []
+let NumGuess = 1
+
+let playGame = true
+
+if(playGame){
+    submit.addEventListener('click', function(e){
+        e.preventDefault()
+        const guess = parseInt(userInput.value)
+        validateGuess(guess)
+    })
+}
+
+function validateGuess(guess){
+     
+}
+
+function checkGuess(guess){
+
+}
+
+function displayGuess(guess){
+
+}
+
+function displayMessage(message){
+
+}
+
+function newGame(){
+
+}
+
+function endGame(){
+
+}
