1951ca4ae81c2c38c68c62e61f801478bfb45fde
Aquib Iqbal <aquibiqbal65@gmail.com>
2024-03-26 20:36:45 +0000

    random guess 1st phase

---
+++ 08_GuessTheNumber/javascript.js
@@ -0,0 +1,7 @@
+const Random = parseInt(Math.random()*100+1);
+
+const submit=document.querySelector('#subt');
+const userInput = document.querySelector('#guessField');
+const guessSlot = document.querySelector('.guesses');
+const remaining = document.querySelector('.lastResult');
+
