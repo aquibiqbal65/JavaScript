ee9750be838e9a5236c19fc73a7ce939ff965d92
aquibiqbal65 <aquibiqbal65@gmail.com>
2024-04-14 10:09:29 +0530

    async handling using try catch

--- 12_Advanced_one/Promises.js
+++ 12_Advanced_one/Promises.js
@@ -1,6 +1,6 @@
 const promiseone = new Promise(function(resolve, reject){
     setTimeout(function(){
-        cosole.log('Async tast is completed');
+        console.log('Async tast is completed');
         resolve()
     }, 1000)
 })
@@ -31,3 +31,72 @@ promiseThree = new Promise(function(resolve, reject){
 promiseThree.then(function(user){
     console.log(user);
 })
+
+
+// promiseFour---------------------------------------------
+
+const promiseFour = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        let error = true
+        if(error){
+            resolve({username: "Aquib", password: "123"})
+        }
+
+        else{
+            reject('ERROR: Something went wrong')
+        }
+    }, 1000);
+
+})
+
+promiseFour.then(function(user){
+    return user.username
+}).then(function(username){        //--------chaining in promises
+    console.log(username);
+}).catch(function(error){
+    console.log(error);
+}).finally(function(){
+    console.log("the promise is eother resolved or rejected")
+})
+
+
+
+
+//promiseFive--------------------------------------------
+
+const promiseFive = new Promise(function(resolve, reject){
+    setTimeout(() => {
+        let error = true
+        if(error){
+            resolve({username: "javascript", password: "123"})
+        }
+
+        else{
+            reject('ERROR: JS went wrong')
+        }
+    }, 1000);
+})
+
+// error handling using try catch-----------
+async function consumepromisefive(){
+    try{
+        const response = await promiseFive
+    console.log(response);
+    }
+    catch{
+        console.log(error);
+    }
+}
+
+// ----------------------------------------------------
+
+async function getAllUsers(){
+    try {
+        const response = await fetch('https://jsonplaceholder.typicode.com/users')
+        const data = await response.json()
+        console.log(data);
+    } catch (error) {
+        console.log("E: ",error);
+    }
+}
+getAllUsers()
